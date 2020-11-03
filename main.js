7a5140e036bcd2c40efe9308450c2572ab03717b
alexanderalban <albanalexander@gmail.com>
2020-11-02 22:39:28 -0600

    initial, checkForWin makes sense, need to figure out start

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const rl = readline.createInterface({
 });
 
 // An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
+// * each key is an array of Numbers: 
+// * A is the far-left, 
+// * B is the middle, 
+// * C is the far-right stack
+// * Each number represents the largest to smallest tokens: 
+// * 4 is the largest, 
+// * 1 is the smallest
 
 let stacks = {
   a: [4, 3, 2, 1],
@@ -32,10 +32,14 @@ const printStacks = () => {
 // Next, what do you think this function should do?
 const movePiece = () => {
   // Your code here
+  if (isLegal() === true) {
+
+  }
 
 }
 
-// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
+// Before you move, should you check if the move it actually allowed? Should 3 be able to be 
+// stacked on 2
 const isLegal = () => {
   // Your code here
 
@@ -44,7 +48,13 @@ const isLegal = () => {
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // Your code here
-
+  for (let i = 1; i < 3; i++);
+  if (stacks.i === [4, 3, 2, 1]) {
+    console.log("You win!")
+    return true;
+  } else {
+    getPrompt;
+  }
 }
 
 // When is this function called? What should it do with its argument?
